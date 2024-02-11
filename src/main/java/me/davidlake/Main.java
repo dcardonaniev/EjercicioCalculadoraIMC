1a73b2c05f0418d876d1f6487bef4a020509391a
David Cardona <david.cardonanieves@mercadolibre.com.com>
2024-02-11 16:00:06 -0500

    Solucion ejercicio

---
+++ src/main/java/me/davidlake/Main.java
@@ -0,0 +1,44 @@
+package me.davidlake;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona personaConstructorVacio = new Persona();
+        Persona personaConstructorParcial = new Persona(
+                "David",
+                21,
+                "1000"
+        );
+        Persona personaConstructorCompleto = new Persona(
+                "David",
+                21,
+                "1000",
+                75.0,
+                1.87
+        );
+
+        if(personaConstructorCompleto.esMayorDeEdad()){
+            System.out.printf("La persona llamada %s es mayor de edad.\n", personaConstructorCompleto.getNombre());
+        } else {
+            System.out.printf("La persona llamada %s es menor de edad.\n", personaConstructorCompleto.getNombre());
+        }
+
+        String estado;
+        switch(personaConstructorCompleto.calcularIMC()) {
+            case -1 -> {
+                estado = "Bajo peso";
+            }
+            case 0 -> {
+                estado = "Peso saludable";
+            }
+            case 1 -> {
+                estado = "Sobrepeso";
+            }
+            default -> {
+                estado = "Desconocido";
+            }
+        }
+
+        System.out.printf("Esta persona tiene un estado %s.\n", estado);
+        System.out.printf("Info: %s", personaConstructorCompleto.toString());
+    }
+}
